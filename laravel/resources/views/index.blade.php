982e5d4b905d8e8116a55f07a725f2183ff55955
Axel-Brosset <axel.brosset@viacesi.fr>
2019-11-08 15:38:47 +0100

    Hello World

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-tofit=no" />
+    <title>BDE CESI Bordeaux</title>
+    <link rel="stylesheet" href="css/index.css" />
+</head>
+
+<body>
+    <h1 class="Test">Hello World</h1>
+</body>
