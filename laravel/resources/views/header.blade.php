fd6a72e9d50c3099251a7e9afaf9731013130251
Axel-Brosset <axel.brosset@viacesi.fr>
2019-11-08 15:31:54 +0100

    LaravelFIX 2.0

---
+++ laravel/resources/views/header.blade.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="css/app.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+
+<header class="header">
+<nav class="navbar">
+    <img src="https://www.cesi.fr/wp-content/uploads/2018/09/cesi-logo.png" class="navbar navbar__logo"/>
+    <p>BDE BORDEAUX</p>
+    <div class="navbar navbar--right">
+        {{$notification}}
+        {{$search}}
+        {{$account}} 
+    </div>
+</nav>
+</header>
+
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/header', function () {
+    return view('headerHandler');
+});
