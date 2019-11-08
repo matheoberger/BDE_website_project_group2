cbb1c01f57855e8c0260aded2728724c3f52e25b
Cnsbmkpsr <remib33@gmail.com>
2019-11-08 17:05:54 +0100

    Add essential

---
+++ laravel/resources/views/event.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>||Working with elements||</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="css/event.css" />
+  </head>
+  <body>
+    <header>Header</header>
+    <div class="main">
+    <nav>
+    navbar
+    </nav>
+    <div class="section">
+<section>Section <article>Article
+</article>
+<div class="aside">
+<aside>Aside</aside>
+<aside>Aside</aside>
+</div>
+</section>
+
+
+<section>Section 2
+<div class="aside">
+
+<Aside>Aside</Aside>
+</div>
+</section>
+</div>
+    </div>
+
+    <footer>Footer</footer>
+  </body>
+</html>
