969a0b2dd2167e25f086f70a8a8e9313ceb78a53
TienYou-Nathan <legonzaur@live.fr>
2019-11-10 16:10:01 +0100

    responsive header

--- laravel/resources/views/header.blade.php
+++ laravel/resources/views/header.blade.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type="text/css" href="css/header.css">
 
 <header class="header">
-<nav class="navbar">
+<nav class="navbar navbar--desktop">
     <img src="https://www.cesi.fr/wp-content/uploads/2018/09/cesi-logo.png" class="navbar navbar__logo"/>
     <h1>BDE BORDEAUX</h1>
     <div class="navbar navbar--right">
@@ -10,6 +10,11 @@
         {{$search}}
         {{$account}} 
     </div>
+    
+</nav>
+<nav class="navbar navbar--mobile">
+    <img src="/images/hamburger.png" class=/>
+    <h1>BDE BORDEAUX</h1>
 </nav>
 </header>
 
