3f9e074a27928abf63750fbe4c3b5a00fe4ba14c
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2019-11-12 11:20:25 +0100

    Navbar  + css animation

--- laravel/resources/views/navbar.blade.php
+++ laravel/resources/views/navbar.blade.php
@@ -5,11 +5,14 @@
 
 <nav class="sidebar sidebar--desktop">
     <img src="https://media.discordapp.net/attachments/637676594768510978/642315646272536576/logo_BDE_1.png?width=702&height=702" class="sidebar sidebar__logo"/>
+    <a href="/html/">LINK1</a>
+    <a href="/html/">LINK2</a>
+    <a href="/html/">LINK3</a>
 </nav>
 
 <nav class="sidebar sidebar--mobile">
-    <img src="/images/hamburger.png" class=/>
-    <h1>BDE BORDEAUX</h1>
+
+
 </nav>
 
 
