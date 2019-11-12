e334ffbe42871a37281cdfa4a91be89074d2fd49
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2019-11-12 09:46:05 +0100

    Responsive sidebar

--- laravel/resources/views/navbar.blade.php
+++ laravel/resources/views/navbar.blade.php
@@ -5,10 +5,8 @@
 
 <nav class="sidebar sidebar--desktop">
     <img src="https://media.discordapp.net/attachments/637676594768510978/642315646272536576/logo_BDE_1.png?width=702&height=702" class="sidebar sidebar__logo"/>
-    
-
-    
 </nav>
+
 <nav class="sidebar sidebar--mobile">
     <img src="/images/hamburger.png" class=/>
     <h1>BDE BORDEAUX</h1>
