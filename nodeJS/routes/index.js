42f704303cb09b88ce54ca1f6f64c5c055849e1d
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2019-11-13 15:47:54 +0100

    updating event API

--- nodeJS/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  route: "/index",
-  get: (req, res) => {
-    res.send("Hello World!");
-  },
-  post: (req, res) => {},
-  put: (req, res) => {},
-  delete: (req, res) => {}
-};
