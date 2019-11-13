b424f029e9a5e9f471fd2762ce6c128e872e2a58
Axel-Brosset <axel.brosset@viacesi.fr>
2019-11-13 14:52:52 +0100

    comment

--- laravel/public/js/index.js
+++ laravel/public/js/index.js
@@ -94,6 +94,7 @@
 /***/ (function(module, exports) {
 
 window.verifForm = function (form) {
+  //lancer les différents tests poru vérifier les champs du formulaire
   if (verifMail(form.email) && verifObject(form.object) && verifText(form.subject)) return true;else {
     alert("Veuillez remplir correctement tous les champs");
     return false;
@@ -104,9 +105,12 @@ window.verifMail = function (champ) {
   isAnEmail = false;
 
   for (var j = 1; j < champ.value.length; j++) {
+    //chercher le caractère @
     if (champ.value.charAt(j) == "@") {
+      //verifier qu'il y a au moins 4 caractère dérrière @
       if (j < champ.value.length - 4) {
         for (var k = j; k < champ.value.length - 2; k++) {
+          //verifier qu'il y a un point derière @
           if (champ.value.charAt(k) == ".") {
             isAnEmail = true;
           }
--- laravel/resources/js/index.js
+++ laravel/resources/js/index.js
@@ -1,4 +1,5 @@
 window.verifForm = function(form) {
+    //lancer les différents tests poru vérifier les champs du formulaire
     if (
         verifMail(form.email) &&
         verifObject(form.object) &&
@@ -14,9 +15,12 @@ window.verifForm = function(form) {
 window.verifMail = function(champ) {
     isAnEmail = false;
     for (var j = 1; j < champ.value.length; j++) {
+        //chercher le caractère @
         if (champ.value.charAt(j) == "@") {
+            //verifier qu'il y a au moins 4 caractère dérrière @
             if (j < champ.value.length - 4) {
                 for (var k = j; k < champ.value.length - 2; k++) {
+                    //verifier qu'il y a un point derière @
                     if (champ.value.charAt(k) == ".") {
                         isAnEmail = true;
                     }
