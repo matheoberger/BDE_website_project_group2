fd5134d8ca1e4d122219bbc79a7626d2cbe636c7
TienYou-Nathan <legonzaur@live.fr>
2019-11-15 23:37:37 +0100

    Navbar reponsive

--- laravel/resources/views/partials/navbar.blade.php
+++ laravel/resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="sidebar sidebar--desktop">
+<nav class="sidebar sidebar--desktop" id="js-navbar">
     <a href="/"><img
             src="https://media.discordapp.net/attachments/637676594768510978/642315646272536576/logo_BDE_1.png?width=702&height=702"
             class="sidebar sidebar__logo" /></a>
@@ -8,3 +8,4 @@
 </nav>
 
 </nav>
+<script src="/js/navbar.js"></script>
