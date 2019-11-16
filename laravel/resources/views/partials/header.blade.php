407067a48172a049b043d89a16ee0c50a43698e1
Axel-Brosset <axel.brosset@viacesi.fr>
2019-11-16 11:35:28 +0100

    maj W3C header

--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -1,28 +1,27 @@
 <header class="header">
     <nav class="customNavbar customNavbar--desktop">
         <a href="https://www.cesi.fr">
-            <img src="https://www.cesi.fr/wp-content/uploads/2018/09/cesi-logo.png"
-                class="customNavbar customNavbar__logo" />
+            <img src="https://www.cesi.fr/wp-content/uploads/2018/09/cesi-logo.png" class="customNavbar customNavbar__logo" alt="cesi_logo" />
         </a>
         <h1>BDE CESI BORDEAUX</h1>
         <div class="customNavbar customNavbar--right">
 
-            <img src="/images/bell.png" class="customNavbar customNavbar__notification" />
+            <img src="/images/bell.png" class="customNavbar customNavbar__notification" alt="notification_bell" />
 
             <input type="text" class="customNavbar customNavbar_search">
-            <img src="/images/panier.png" class="customNavbar customNavbar__panier" />
+            <img src="/images/panier.png" class="customNavbar customNavbar__panier" alt="basket_image" />
             <?php if (session()->has('firstname') && session()->has('lastname')) {
                 echo "<a class='customNavbar customNavbar__user' href='/disconnect'> Se déconnecter </a>";
             } else {
                 echo "  <a class='customNavbar customNavbar__user' href='/login'> Se connecter </a>";
             }
             ?>
-            <script type="text/javascript">
+            <script>
                 // document.cookie = "accept_cookie = cookie ; expires=" + new Date("1970-01-01").toUTCString() + "; path=/";
             </script>
             <?php
             if (!isset($_COOKIE['accept_cookie'])) {
-                echo " <script type='text/javascript' src='js/cookie.js'></script> ";
+                echo " <script src='js/cookie.js'></script> ";
             }
 
             ?>
