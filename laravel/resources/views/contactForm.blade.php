604920d0ba2e695c1bb6ec0bb1874e9ab039ab5a
Axel-Brosset <axel.brosset@viacesi.fr>
2019-11-16 16:11:09 +0100

    fix Contact + W3C contact

--- laravel/resources/views/contactForm.blade.php
+++ laravel/resources/views/contactForm.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
 
@@ -12,7 +12,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/index.js"></script>
+    <script src="js/index.js"></script>
     <link rel="stylesheet" href="css/app.css" />
     <link rel="stylesheet" type="text/css" href="css/header.css">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
@@ -27,41 +27,38 @@
         @include("partials/navbar")
         <div class="conteneur">
             <div class="contenu">
+                <section>
+                    <h1 class="formTitle">Vous avez des questions ou des idées ? Contactez nous !</h1>
+                    <div>
+                        <form method="post" onsubmit="return verifForm(this)">
+                            @csrf
+                            <!-- Champ Email -->
+                            <label for="email">Email :</label>
+                            <input type="text" id="email" name="email" placeholder="Entrez votre adresse E-mail" required="required" <?php if (isset($_POST['email'])) {
+                                                                                                                                            echo "value='" . $_POST['email'] . "'";
+                                                                                                                                        }; ?>>
+                            <!-- Champ Objet -->
+                            <label for="subject">Objet (5 - 50 caractères) :</label>
+                            <input type="text" id="subject" name="subject" placeholder=" Entrez l'objet de votre message" required="required" <?php if (isset($_POST['subject'])) {
+                                                                                                                                                    echo "value='" . $_POST['subject'] . "'";
+                                                                                                                                                }; ?>>
+                            <!-- Champ Texte -->
+                            <label for="message">Message (min 50 caractères) :</label>
+                            <textarea id="message" name="message" placeholder=" Ecrivez votre message..." required="required"><?php if (isset($_POST['message'])) {
+                                                                                                                                    echo $_POST['message'];
+                                                                                                                                }; ?></textarea>
+                            <?php if (isset($error) && isset($color)) {
 
-                <body>
-                    <section>
-                        <h1 class="formTitle">Vous avez des questions ou des idées ? Contactez nous !</h1>
-                        <div>
-                            <form method="post" onsubmit="return verifForm(this)">
-                                @csrf
-                                <!-- Champ Email -->
-                                <label for="email">Email :</label>
-                                <input type="text" id="email" name="email" placeholder="Entrez votre adresse E-mail" required="required" <?php if (isset($_POST['email'])) {
-                                                                                                                                                echo "value='" . $_POST['email'] . "'";
-                                                                                                                                            }; ?>>
-                                <!-- Champ Objet -->
-                                <label for="subject">Objet (5 - 50 caractères) :</label>
-                                <input type="text" id="subject" name="subject" placeholder=" Entrez l'objet de votre message" required="required" <?php if (isset($_POST['subject'])) {
-                                                                                                                                                        echo "value='" . $_POST['subject'] . "'";
-                                                                                                                                                    }; ?>>
-                                <!-- Champ Texte -->
-                                <label for="message">Message (min 50 caractères) :</label>
-                                <textarea id="message" name="message" placeholder=" Ecrivez votre message..." required="required"><?php if (isset($_POST['message'])) {
-                                                                                                                                        echo $_POST['message'];
-                                                                                                                                    }; ?></textarea>
-                                <?php if (isset($error) && isset($color)) {
-
-                                    echo "<p class='errorMessage' style='color:$color'>" . $error . "</p>";
-                                };
-                                ?>
-                                <!-- Bouton envoyer -->
-                                <input class="submit_button" type="submit" value="Envoyer">
-                            </form>
-                        </div>
-                    </section>
-                    @include("partials/footer")
+                                echo "<p class='errorMessage' style='color:$color'>" . $error . "</p>";
+                            };
+                            ?>
+                            <!-- Bouton envoyer -->
+                            <input class="submit_button" type="submit" value="Envoyer">
+                        </form>
+                    </div>
+                </section>
+                @include("partials/footer")
             </div>
-        </div>
         </div>
     </main>
 </body>
