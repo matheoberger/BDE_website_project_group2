6a2f8ef4d5436098aff81433b22b85ae3667de8f
Cnsbmkpsr <remib33@gmail.com>
2019-11-16 20:39:54 +0100

    Mise à jour W3C

--- laravel/resources/views/event.blade.php
+++ laravel/resources/views/event.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>Event</title>
-    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <link rel="stylesheet" href="/css/app.css" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
