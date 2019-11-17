f66e9759c63f9c2c088de4f6385b20bb85f6a398
Cnsbmkpsr <remib33@gmail.com>
2019-11-17 23:25:38 +0100

    W3C Correctif + comments

--- laravel/resources/views/eventType.blade.php
+++ laravel/resources/views/eventType.blade.php
@@ -31,7 +31,7 @@ if (session('role')) {
 <html lang="fr">
 
 <head>
-    <title>Event Type</title>
+    <title>Events</title>
     <meta charset="UTF-8" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
