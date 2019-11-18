658e6c2cbc94eb8d488dc2c43488131997b744d6
Cnsbmkpsr <remib33@gmail.com>
2019-11-18 17:12:35 +0100

    reverse

--- laravel/public/js/insertDataToEvent.js
+++ laravel/public/js/insertDataToEvent.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -275,7 +275,7 @@ $.get("http://localhost:3000/event/".concat(id), function (data, status) {
 
 /***/ }),
 
-/***/ 4:
+/***/ 3:
 /*!*************************************************!*\
   !*** multi ./resources/js/insertDataToEvent.js ***!
   \*************************************************/
