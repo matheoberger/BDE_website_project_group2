b35c33ae1deb120cf0263cd6f54280b858bb5b71
matheoberger <matheo.berger@viacesi.fr>
2019-11-18 01:46:03 +0100

    correct navbar on event

--- laravel/resources/views/event.blade.php
+++ laravel/resources/views/event.blade.php
@@ -2,43 +2,49 @@
 <html lang="fr">
 
 <head>
-  <title>Event</title>
-  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-  <link rel="stylesheet" href="/css/app.css" />
+    <title>Event</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="/css/header.css">
-  <link rel="stylesheet" type="text/css" href="/css/navbar.css">
-  <link rel="stylesheet" href="/css/event.css" />
-  <link rel="stylesheet" href="/css/header.css" />
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    <link rel="stylesheet" href="/css/event.css" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 
 <body>
-  @include('partials/header')
-
-  <div>
-    @include("partials/navbar")
+    @include('partials/header')
     <main>
+        @include("partials/navbar")
+        <div class="conteneur">
+            <div class="contenu">
+                <div class="panier__body">
 
-      <div class="conteneur">
-        <div>
-          <h2> <u>Les évènements</u></h2>
-        </div>
-        <?php
-        if (session('role') == 'Administrator') {
-          echo "<form action='/newEvent' method='get'>
+                    <div>
+                        <h2> <u>Les évènements</u></h2>
+                    </div>
+                    <?php
+          if (session('role') == 'Administrator') {
+            echo "<form action='/newEvent' method='get'>
                             <button class='btn add_event'>Ajouter un event</button>
                     </form> ";
-        };
-        ?>
-        <div id="js-contenair_event"></div>
-      </div>
-
-      <script src="/js/insertEvent.js"></script>
+          };
+          ?>
+                    <div id="js-contenair_event"></div>
+                </div>
+            </div>
+            <script src="/js/insertEvent.js"></script>
+        </div>
+        </div>
+        </div>
     </main>
-  </div>
 </body>
 
 </html>
