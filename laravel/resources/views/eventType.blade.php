df1dad03379aee47699f03a6e070eb152a5a9a49
Cnsbmkpsr <remib33@gmail.com>
2019-11-18 01:51:05 +0100

    Update navbar

--- laravel/resources/views/eventType.blade.php
+++ laravel/resources/views/eventType.blade.php
@@ -29,47 +29,36 @@ if (session('role')) {
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <title>Events</title>
     <meta charset="UTF-8" />
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/eventType.css" />
     <link rel="stylesheet" href="/css/header.css" />
-
-
-
-
 </head>
 
 <body>
-
     @include('/partials/header')
-
+    @include('partials/navbar')
     <main>
-
         <div>
             <h2><?php echo $event[0]["title_events"] ?></h2>
         </div>
         <div class="conteneur">
-
             <section>
                 <article>
                     <h3><?php echo $event[0]["place"] ?></h3>
                     <p><?php echo $event[0]["description"] ?></p>
                 </article>
                 <div class="picture_gallery" id="js-picture-gallery"><br>
-
                 </div>
                 <aside>Pannel event :
                     <br>
                     <?php echo "<form action='/addpicture/$id'>
                         <button type='submit' class='btn add_picture'>Ajouter photo </button>
                     </form>" ?>
-
                     <?php
                     if (session('role') == 'Administrator') {
                         echo "<form action='/event/$id/edit/' method='get'><button class='btn edit_event'>Modifier event</button></form>";
@@ -79,7 +68,6 @@ if (session('role')) {
                     };
                     ?>
                     <?php
-
                     if (empty($isRegistered)) {
                         echo '
                     <form action="/event/participate" method="post">
@@ -103,26 +91,17 @@ if (session('role')) {
                     </select>
                     <button type="submit" class="btn download">Télécharger</button>
                 </form>'
-
                     ?>
-
                 </aside>
-
-
-
             </section>
-
         </div>
-
     </main>
     <script>
         var id = <?php echo $id ?>;
         <?php
-
         if (session('role') == 'Moderator') {
             echo 'var button = "<button class=' . "'btn warning'" . '>Signaler</button>";';
         }
-
         if (session('role') == 'Administrator') {
             echo 'var button = "<button class=' . "'btn delete'" . '>Supprimer</button>";';
         }
@@ -138,7 +117,6 @@ if (session('role')) {
         if (empty($isRegistered)) {
             echo 'var registered = false;';
         }
-
         ?>
     </script>
     <script src="/js/insertDataToEvent.js">
